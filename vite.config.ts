0c9e68b6305670b759c551948b9a2547c73a074c
Mikel Giffin <giffinmike@hotmail.com>
2024-06-01 10:42:50 -0700

    fix: now able to run over https for access on iphone

--- vite.config.ts
+++ vite.config.ts
@@ -45,9 +45,44 @@
 
 ///////////////////////////////////////
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+// import fs from "fs";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   server: {
+//     host: "0.0.0.0",
+//     port: 5173,
+//     https: {
+//       key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
+//       cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
+//     },
+//   },
+//   build: {
+//     outDir: "dist",
+//   },
+// });
+
+//////////////////////////////////////
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import fs from "fs";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Determine whether to use HTTPS
+const useHttps = process.env.VITE_USE_HTTPS === "true";
 
 export default defineConfig({
   plugins: [react()],
@@ -59,6 +94,12 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
+    https: useHttps
+      ? {
+        key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
+        cert: fs.readFileSync(path.resolve(__dirname, "cert.pem")),
+      }
+      : undefined,
   },
   build: {
     outDir: "dist",
