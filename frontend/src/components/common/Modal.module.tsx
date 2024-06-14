7ce971855a253efd2585a16a63dc1d675603aa1d
Mikel Giffin <giffinmike@hotmail.com>
2024-06-14 09:34:32 -0700

    wip: modal extraction1

---
+++ frontend/src/components/common/Modal.module.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./Modal.module.css";
+
+interface ModalProps {
+  isVisible: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  title: string;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isVisible,
+  onClose,
+  children,
+  title,
+}) => {
+  if (!isVisible) return null;
+
+  const handleOverlayClick = (e: React.MouseEvent) => {
+    if ((e.target as Element).classList.contains(styles.modalOverlay)) {
+      onClose();
+    }
+  };
+
+  return (
+    <div className={styles.modalOverlay} onClick={handleOverlayClick}>
+      {" "}
+      <div className={styles.modalContent}>
+        {" "}
+        <div className={styles.closeButton}>
+          {" "}
+          <button className={styles.closeButtonBtn} onClick={onClose}>
+            {" "}
+            X{" "}
+          </button>{" "}
+        </div>{" "}
+        <h2 className={styles.h2}> {title}</h2> {children}
+      </div>{" "}
+    </div>
+  );
+};
+
+export default Modal;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./frontend/src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // Enable dark mode with a specific class
+  darkMode: "class", // Use 'media' or 'class' for dark mode
   theme: {
     extend: {
       // Custom Colors
@@ -21,14 +21,39 @@ export default {
 
       // Custom Spacing
       spacing: {
+        1: "0.25rem", // 4px
+        2: "0.5rem", // 8px
+        3: "0.75rem", // 12px
+        4: "1rem", // 16px
+        5: "1.25rem", // 20px
+        6: "1.5rem", // 24px
+        7: "1.75rem", // 28px
+        8: "2rem", // 32px
+        9: "2.25rem", // 36px
+        10: "2.5rem", // 40px
+        12: "3rem", // 48px
+        14: "3.5rem", // 56px
+        16: "4rem", // 64px
+        20: "5rem", // 80px
+        24: "6rem", // 96px
+        28: "7rem", // 112px
+        32: "8rem", // 128px
+        36: "9rem", // 144px
+        40: "10rem", // 160px
+        44: "11rem", // 176px
+        48: "12rem", // 192px
+        52: "13rem", // 208px
+        56: "14rem", // 224px
+        60: "15rem", // 240px
+        64: "16rem", // 256px
         72: "18rem", // 288px
-        84: "21rem", // 336px
+        80: "20rem", // 320px
         96: "24rem", // 384px
         128: "32rem", // 512px
         144: "36rem", // 576px
       },
 
-      // Custom Margin and Padding
+      // Custom Margin
       margin: {
         auto: "auto",
         1: "0.25rem", // 4px
@@ -62,6 +87,8 @@ export default {
         128: "32rem", // 512px
         144: "36rem", // 576px
       },
+
+      // Custom Padding
       padding: {
         1: "0.25rem", // 4px
         2: "0.5rem", // 8px
@@ -157,12 +184,12 @@ export default {
 
       // Custom Line Heights
       lineHeight: {
-        none: "1", // 1
-        tight: "1.25", // 1.25
-        snug: "1.375", // 1.375
-        normal: "1.5", // 1.5
-        relaxed: "1.625", // 1.625
-        loose: "2", // 2
+        none: "1",
+        tight: "1.25",
+        snug: "1.375",
+        normal: "1.5",
+        relaxed: "1.625",
+        loose: "2",
       },
 
       // Custom Letter Spacing
