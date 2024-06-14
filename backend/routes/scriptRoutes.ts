5f6d2d1405f2bb786ed7aa130479800ae0ea0568
Matt Birnholtz <mattbirn93@gmail.com>
2024-06-14 17:26:17 -0400

    fixed the update script routes

--- backend/routes/scriptRoutes.ts
+++ backend/routes/scriptRoutes.ts
@@ -1,14 +1,11 @@
 import express from 'express';
 import {
   fetchScripts,
-  createScript,
-  fetchScriptsById,
 } from '../controllers/scriptController.js';
 
 const router = express.Router();
 
 router.get('/', fetchScripts);
-router.get('/fetchScriptsById', fetchScriptsById);
-router.post('/createNewScript', createScript);
+
 
 export default router;
