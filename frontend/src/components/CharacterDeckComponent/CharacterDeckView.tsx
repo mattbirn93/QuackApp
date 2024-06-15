164d2da4fe62c20ab2ee0022ee8f8886f2675524
Matt Birnholtz <mattbirn93@gmail.com>
2024-06-15 02:05:26 -0400

    updated so much

--- frontend/src/components/CharacterDeckComponent/CharacterDeckView.tsx
+++ frontend/src/components/CharacterDeckComponent/CharacterDeckView.tsx
@@ -2,12 +2,23 @@ import React from "react";
 import CharacterDeckComponent from "./CharacterDeckComponent";
 import styles from "./CharacterDeckView.module.css";
 
-const CharacterDeckView = () => {
+interface CharacterDeckViewProps {
+  characterArray: string[];
+  onCharacterButtonClick: (character: string) => void;
+}
+
+const CharacterDeckView: React.FC<CharacterDeckViewProps> = ({
+  characterArray,
+  onCharacterButtonClick,
+}) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.mainContainer}>
         <div className={styles.characterDeck}>
-          <CharacterDeckComponent />
+          <CharacterDeckComponent
+            characterArray={characterArray}
+            onCharacterButtonClick={onCharacterButtonClick}
+          />
         </div>
       </div>
     </div>
