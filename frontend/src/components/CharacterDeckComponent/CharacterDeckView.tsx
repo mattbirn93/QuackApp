b198d5ec3d547b5d322bcbf493464725e961988c
Mikel Giffin <giffinmike@hotmail.com>
2024-06-20 15:50:27 -0700

    wip: reisze handle implemented and formatting for mobile in progress2

--- frontend/src/components/CharacterDeckComponent/CharacterDeckView.tsx
+++ frontend/src/components/CharacterDeckComponent/CharacterDeckView.tsx
@@ -11,12 +11,16 @@ const CharacterDeckView: React.FC<CharacterDeckViewProps> = ({
   characterArray,
   onCharacterButtonClick,
 }) => {
-  const [height, setHeight] = useState(150);
+  const [height, setHeight] = useState(150); // Initial height of the characterDeck in pixels
   const isResizing = useRef(false);
 
   const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
     isResizing.current = true;
     document.body.style.cursor = "ns-resize";
+  };
+
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    isResizing.current = true;
   };
 
   const handleMouseMove = (e: MouseEvent) => {
@@ -27,18 +31,35 @@ const CharacterDeckView: React.FC<CharacterDeckViewProps> = ({
     }
   };
 
+  const handleTouchMove = (e: TouchEvent) => {
+    if (!isResizing.current) return;
+    const touch = e.touches[0];
+    const newHeight = window.innerHeight - touch.clientY;
+    if (newHeight >= 80 && newHeight <= 320) {
+      setHeight(newHeight);
+    }
+  };
+
   const handleMouseUp = () => {
     isResizing.current = false;
     document.body.style.cursor = "default";
   };
 
+  const handleTouchEnd = () => {
+    isResizing.current = false;
+  };
+
   useEffect(() => {
     document.addEventListener("mousemove", handleMouseMove);
     document.addEventListener("mouseup", handleMouseUp);
+    document.addEventListener("touchmove", handleTouchMove);
+    document.addEventListener("touchend", handleTouchEnd);
 
     return () => {
       document.removeEventListener("mousemove", handleMouseMove);
       document.removeEventListener("mouseup", handleMouseUp);
+      document.removeEventListener("touchmove", handleTouchMove);
+      document.removeEventListener("touchend", handleTouchEnd);
     };
   }, []);
 
@@ -46,7 +67,12 @@ const CharacterDeckView: React.FC<CharacterDeckViewProps> = ({
     <div className={styles.wrapper}>
       <div className={styles.mainContainer}>
         <div className={styles.characterDeck} style={{ height: `${height}px` }}>
-          <div className={styles.resizeHandle} onMouseDown={handleMouseDown}>
+          <div
+            className={styles.resizeHandle}
+            onMouseDown={handleMouseDown}
+            onTouchStart={handleTouchStart}
+          >
+            <div className={styles.resizeIcon}></div>
             <span className={styles.plusSign}>+</span>
           </div>
           <CharacterDeckComponent
