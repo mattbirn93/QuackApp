86c7172f6d77cb2a10801063346ea7836793fd41
Mikel Giffin <giffinmike@hotmail.com>
2024-06-21 12:32:41 -0700

    wip: scripts library styled up more2

--- frontend/src/views/ScriptsLibraryView/ScriptsLibraryView.tsx
+++ frontend/src/views/ScriptsLibraryView/ScriptsLibraryView.tsx
@@ -7,26 +7,26 @@ import styles from "./ScriptsLibraryView.module.css";
 const ScriptsLibraryView: React.FC = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.searchBarContainer}>
-        <div className={styles.searchBarWrapper}>
-          <FontAwesomeIcon icon={faSearch} className={styles.searchIcon} />
-          <input
-            type="text"
-            placeholder="Search"
-            className={styles.searchBar}
-          />
-        </div>
-        <div className={styles.signInButtonContainer}>
-          <button className={styles.signInButton}>
-            <FontAwesomeIcon
-              icon={faUser}
-              className={styles.signInButtonIcon}
-            />
-            Sign In
-          </button>
-        </div>
-      </div>
       <div className={styles.mainContainer}>
+        <div className={styles.searchBarContainer}>
+          <div className={styles.searchBarWrapper}>
+            <FontAwesomeIcon icon={faSearch} className={styles.searchIcon} />
+            <input
+              type="text"
+              placeholder="Search"
+              className={styles.searchBar}
+            />
+          </div>
+          <div className={styles.signInButtonContainer}>
+            <button className={styles.signInButton}>
+              <FontAwesomeIcon
+                icon={faUser}
+                className={styles.signInButtonIcon}
+              />
+              Sign In
+            </button>
+          </div>
+        </div>
         <div className={styles.topContainer}>
           <h1 className={styles.heading1}>Scripts Library View</h1>
           <h2 className={styles.heading2}>Open up a script to get started</h2>
