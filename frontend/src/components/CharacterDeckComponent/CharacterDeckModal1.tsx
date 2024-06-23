222bba2d1fb0ebca006eb586277c09dd99111423
Mikel Giffin <giffinmike@hotmail.com>
2024-06-22 22:19:16 -0700

    fix: pwa mode accessing server on phone now

--- frontend/src/components/CharacterDeckComponent/CharacterDeckModal1.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CharacterDeckModal1 = () => {
-  return <div>CharacterDeckModal1</div>;
-};
-
-export default CharacterDeckModal1;
--- server.ts
+++ server.ts
@@ -181,6 +181,191 @@ server.listen(Number(PORT), "0.0.0.0", () => {
   console.log(`Server running on https://localhost:${PORT}`);
 });
 
+////////////
+
+// import "tsconfig-paths/register.js";
+// import express from "express";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import bodyParser from "body-parser";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import connectDB from "./backend/config2.js";
+// import https from "https";
+// import fs from "fs";
+// import { Server as SocketIOServer } from "socket.io";
+
+// // Importing user-related routes
+// import userRoutes from "./backend/routes/userRoutes.js";
+// import scriptRoutes from "./backend/routes/scriptRoutes.js";
+// import sceneRoutes from "./backend/routes/sceneRoutes.js";
+// import scriptsFullRoutes from "./backend/routes/scriptsFullRoutes.js";
+// import sceneVersionRoutes from "./backend/routes/sceneVersionRoutes.js";
+// import sceneVersionContentRoutes from "./backend/routes/sceneVersionContentRoutes.js";
+// import { createUserSocket } from "./backend/controllers/userController2.js";
+// import { getSceneVersionContentSocket } from "./backend/controllers/sceneVersionContentWSController.js";
+// import { createContentItemSocket } from "./backend/controllers/sceneVersionContentWSController.js";
+// import { updateContentItemSocket } from "./backend/controllers/sceneVersionContentWSController.js";
+// import { deleteContentItemSocket } from "./backend/controllers/sceneVersionContentWSController.js";
+// import {
+//   getCharactersById,
+//   addCharacterToArray,
+//   updateCharacterInArray,
+// } from "./backend/controllers/charactersWSController.js";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// dotenv.config();
+
+// const app = express();
+// const PORT = process.env.PORT || 5001;
+
+// const keyPath =
+//   process.env.VITE_SSL_KEY_PATH || path.join(__dirname, "./certs/key.pem");
+// const certPath =
+//   process.env.VITE_SSL_CERT_PATH || path.join(__dirname, "./certs/cert.pem");
+
+// const httpsOptions = {
+//   key: fs.readFileSync(keyPath),
+//   cert: fs.readFileSync(certPath),
+// };
+
+// const server = https.createServer(httpsOptions, app);
+// const io = new SocketIOServer(server, {
+//   cors: {
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// app.use(bodyParser.json());
+// app.use(express.json());
+
+// app.use(cors());
+// app.use(express.static(path.join(__dirname, "dist")));
+
+// connectDB();
+
+// // Define API Routes
+// app.use("/api/users", userRoutes);
+// app.use("/api/users/fetchUserById", userRoutes);
+// app.use("/api/scripts", scriptRoutes);
+// app.use("/api/scripts/fetchScriptsById", sceneRoutes);
+// app.use("/api/scripts/createNewScript", sceneRoutes);
+// app.use("/api/scenes", sceneRoutes);
+// app.use("/api/scenes/fetchScriptsFull", sceneRoutes);
+// app.use("/api/scenes/updateScriptsContent", sceneRoutes);
+// app.use("/api/scenes/updateScriptTitlePage", sceneRoutes);
+// app.use("/api/scenes/fetchScriptsById", sceneRoutes);
+// app.use("/api/scenes/createNewScript", sceneRoutes);
+// app.use("api/scenes/createSecene", sceneRoutes);
+// app.use("/api/sceneVersions", sceneVersionRoutes);
+// app.use("/api/sceneVersions/updateCurrentVersion", sceneVersionRoutes);
+// app.use("/api/sceneVersions/createSceneVersion", sceneVersionRoutes);
+// app.use("/api/sceneVersionContent", sceneVersionContentRoutes);
+// app.use("/api/scenes/sceneVersions", sceneVersionContentRoutes);
+// app.use("/api/scenes/sceneVersionContent", sceneVersionContentRoutes);
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "dist", "index.html"));
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("New client connected", socket.id);
+
+//   socket.on("add_user", (data) => {
+//     createUserSocket(data, (error: any, savedUser: any) => {
+//       if (error) {
+//         socket.emit("user_add_error", error);
+//       } else {
+//         socket.emit("user_added", savedUser);
+//       }
+//     });
+//   });
+
+//   socket.on("get_scene_version_content", (data) => {
+//     const { id } = data;
+//     getSceneVersionContentSocket(id, (error: any, result: any) => {
+//       if (error) {
+//         socket.emit("get_scene_version_content_error", error);
+//       } else {
+//         socket.emit("scene_version_content", result);
+//       }
+//     });
+//   });
+
+//   socket.on("create_content_item", (data: any) => {
+//     console.log("Received create_content_item event:", data);
+//     createContentItemSocket(data, (error: any, result: any) => {
+//       if (error) {
+//         socket.emit("create_content_item_error", error);
+//       } else {
+//         socket.emit("content_item_created", result);
+//       }
+//     });
+//   });
+
+//   socket.on("update_content_item", (data: any) => {
+//     console.log("Received update_content_item event:", data);
+//     updateContentItemSocket(data, (error: any, result: any) => {
+//       if (error) {
+//         socket.emit("update_content_item_error", error);
+//       } else {
+//         socket.emit("content_item_updated", result);
+//       }
+//     });
+//   });
+
+//   socket.on("delete_content_item", (data: any) => {
+//     console.log("Received delete_content_item event:", data);
+//     deleteContentItemSocket(data, (error: any, result: any) => {
+//       if (error) {
+//         socket.emit("delete_content_item_error", error);
+//       } else {
+//         socket.emit("content_item_deleted", result);
+//       }
+//     });
+//   });
+//   socket.on("getCharactersById", (id) => {
+//     getCharactersById(id, (error: any, characters: any) => {
+//       if (error) {
+//         socket.emit("error", error);
+//       } else {
+//         socket.emit("charactersData", characters);
+//       }
+//     });
+//   });
+
+//   socket.on("addCharacterToArray", (data) => {
+//     addCharacterToArray(data, (error: any, updatedCharacters: any) => {
+//       if (error) {
+//         socket.emit("error", error);
+//       } else {
+//         socket.emit("updatedCharacters", updatedCharacters);
+//       }
+//     });
+//   });
+
+//   socket.on("updateCharacterInArray", (data) => {
+//     updateCharacterInArray(data, (error: any, updatedCharacters: any) => {
+//       if (error) {
+//         socket.emit("error", error);
+//       } else {
+//         socket.emit("updatedCharacters", updatedCharacters);
+//       }
+//     });
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected", socket.id);
+//   });
+// });
+
+// server.listen(Number(PORT), "0.0.0.0", () => {
+//   console.log(`Server running on https://localhost:${PORT}`);
+// });
+
 //////////////////////////////////////////////////
 
 // The server file initializes the Express application, connects to the database, sets up middleware, and defines the routes.
