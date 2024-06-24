322cb41d3e1d2c55ecceb9633f8ddb7c3ef7835b
Mikel Giffin <giffinmike@hotmail.com>
2024-06-24 11:37:42 -0700

    chore: sidebar in progress 11

--- frontend/src/components/SidePanelComponent/SidePanelView.tsx
+++ frontend/src/components/SidePanelComponent/SidePanelView.tsx
@@ -126,6 +126,28 @@ const SidePanelView = ({
           <div className="section">
             <div
               className="sectionHeader"
+              onClick={() => toggleSection("details")}
+            >
+              <span>Notes</span>
+              {expandedSections.details ? <FaChevronUp /> : <FaChevronDown />}
+            </div>
+            {expandedSections.details && (
+              <>
+                <div className="sidebarItem">
+                  <FaHashtag />
+                  <span>Note 1</span>
+                </div>
+                <div className="sidebarItem">
+                  <FaHashtag />
+                  <span>Note 2</span>
+                </div>
+              </>
+            )}
+          </div>
+
+          <div className="section">
+            <div
+              className="sectionHeader"
               onClick={() => toggleSection("characters")}
             >
               <span>Characters</span>
@@ -153,99 +175,3 @@ const SidePanelView = ({
 };
 
 export default SidePanelView;
-
-//////////////////
-
-// import React from "react";
-// import { FaArrowLeft, FaArrowRight, FaHashtag, FaUser } from "react-icons/fa";
-// import { AiOutlinePlus } from "react-icons/ai";
-// import "./sidePanel.css";
-
-// const SidePanelView = ({
-//   isOpen,
-//   isHovered,
-//   toggleSidePanel,
-//   scriptName,
-//   handleMouseEnter,
-//   handleMouseLeave,
-// }: {
-//   isOpen: boolean;
-//   isHovered: boolean;
-//   toggleSidePanel: () => void;
-//   scriptName: string;
-//   handleMouseEnter: () => void;
-//   handleMouseLeave: () => void;
-// }) => {
-//   return (
-//     <div
-//       className={`sidePanel ${isOpen || isHovered ? "open" : "collapsed"}`}
-//       onMouseEnter={handleMouseEnter}
-//       onMouseLeave={handleMouseLeave}
-//     >
-//       <div className="toggleButtonContainer">
-//         <button className="toggleButton" onClick={toggleSidePanel}>
-//           {isOpen ? <FaArrowLeft /> : <FaArrowRight />}
-//         </button>
-//       </div>
-//       {(isOpen || isHovered) && (
-//         <div className="sidePanelContent">
-//           <div className="titleContainer">
-//             <p className="scriptName">{scriptName}</p>
-//           </div>
-//           <div className="section">
-//             <div className="sectionHeader">
-//               <span>Owners</span>
-//               <button className="addButton">
-//                 <AiOutlinePlus />
-//               </button>
-//             </div>
-//             <div className="sidebarItem">
-//               <FaUser />
-//               <span>Mike Giffin</span>
-//             </div>
-//             <div className="sidebarItem">
-//               <FaUser />
-//               <span>Matt Buttholtz</span>
-//             </div>
-//           </div>
-
-//           <div className="section">
-//             <div className="sectionHeader">
-//               <span>Versions</span>
-//               <button className="addButton">
-//                 <AiOutlinePlus />
-//               </button>
-//             </div>
-//             <div className="sidebarItem">
-//               <FaHashtag />
-//               <span>Scene Versions</span>
-//             </div>
-//             <div className="sidebarItem">
-//               <FaHashtag />
-//               <span>Scene Content</span>
-//             </div>
-//           </div>
-
-//           <div className="section">
-//             <div className="sectionHeader">
-//               <span>Script Details</span>
-//               <button className="addButton">
-//                 <AiOutlinePlus />
-//               </button>
-//             </div>
-//             <div className="sidebarItem">
-//               <FaHashtag />
-//               <span>Date Added</span>
-//             </div>
-//             <div className="sidebarItem">
-//               <FaHashtag />
-//               <span>Pages</span>
-//             </div>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default SidePanelView;
