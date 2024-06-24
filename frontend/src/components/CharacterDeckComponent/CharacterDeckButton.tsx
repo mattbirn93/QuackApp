d44d7695a4d5fcddd70f86433849528554db49d2
Mikel Giffin <giffinmike@hotmail.com>
2024-06-24 13:17:28 -0700

    chore: sidebar in progress 14

--- frontend/src/components/CharacterDeckComponent/CharacterDeckButton.tsx
+++ frontend/src/components/CharacterDeckComponent/CharacterDeckButton.tsx
@@ -4,26 +4,68 @@ import styles from "./CharacterDeckButton.module.css";
 interface CharacterDeckButtonProps {
   letter: string;
   onClick: () => void;
+  onDragStart: (character: string) => void;
+  onDragEnd: () => void;
 }
 
 export const CharacterDeckButton: React.FC<CharacterDeckButtonProps> = ({
   letter,
   onClick,
+  onDragStart,
+  onDragEnd,
 }) => {
   return (
-    <button className={styles.characterDeckButton} onClick={onClick}>
+    <button
+      className={styles.characterDeckButton}
+      onClick={onClick}
+      draggable
+      onDragStart={() => onDragStart(letter)}
+      onDragEnd={onDragEnd}
+    >
       {letter}
     </button>
   );
 };
 
-export const CharacterDeckButtonAdd: React.FC<CharacterDeckButtonProps> = ({
-  letter,
-  onClick,
-}) => {
+export const CharacterDeckButtonAdd: React.FC<{
+  letter: string;
+  onClick: () => void;
+}> = ({ letter, onClick }) => {
   return (
     <button className={styles.characterDeckButtonAdd} onClick={onClick}>
       {letter}
     </button>
   );
 };
+
+///////////////////////
+
+// import React from "react";
+// import styles from "./CharacterDeckButton.module.css";
+
+// interface CharacterDeckButtonProps {
+//   letter: string;
+//   onClick: () => void;
+// }
+
+// export const CharacterDeckButton: React.FC<CharacterDeckButtonProps> = ({
+//   letter,
+//   onClick,
+// }) => {
+//   return (
+//     <button className={styles.characterDeckButton} onClick={onClick}>
+//       {letter}
+//     </button>
+//   );
+// };
+
+// export const CharacterDeckButtonAdd: React.FC<CharacterDeckButtonProps> = ({
+//   letter,
+//   onClick,
+// }) => {
+//   return (
+//     <button className={styles.characterDeckButtonAdd} onClick={onClick}>
+//       {letter}
+//     </button>
+//   );
+// };
