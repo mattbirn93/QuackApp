b5c87c35251354b59e10f95853e32f156febe8bc
Mikel Giffin <giffinmike@hotmail.com>
2024-06-24 10:06:11 -0700

    chore: sidebar in progress 6

--- frontend/src/components/SidePanelComponent/SidePanelComponent.tsx
+++ frontend/src/components/SidePanelComponent/SidePanelComponent.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
-import { FaArrowLeft, FaArrowRight, FaHashtag, FaUser } from "react-icons/fa";
-import { AiOutlinePlus } from "react-icons/ai";
-import "./sidePanel.css";
+import SidePanelView from "./SidePanelView";
 
-const SidePanel = ({ scriptName }) => {
+const SidePanel = ({ scriptName }: { scriptName: string }) => {
   const [isOpen, setIsOpen] = useState(true);
   const [isHovered, setIsHovered] = useState(false);
 
@@ -24,56 +22,14 @@ const SidePanel = ({ scriptName }) => {
   };
 
   return (
-    <div
-      className={`sidePanel ${isOpen || isHovered ? "open" : "collapsed"}`}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <div className="toggleButtonContainer">
-        <button className="toggleButton" onClick={toggleSidePanel}>
-          {isOpen ? <FaArrowLeft /> : <FaArrowRight />}
-        </button>
-      </div>
-      {(isOpen || isHovered) && (
-        <div className="sidePanelContent">
-          <div className="titleContainer">
-            <p className="scriptName">{scriptName}</p>
-          </div>
-          <div className="section">
-            <div className="sectionHeader">
-              <span>Owners</span>
-              <button className="addButton">
-                <AiOutlinePlus />
-              </button>
-            </div>
-            <div className="sidebarItem">
-              <FaUser />
-              <span>Mike Giffin</span>
-            </div>
-            <div className="sidebarItem">
-              <FaUser />
-              <span>Matt Buttholtz</span>
-            </div>
-          </div>
-          <div className="section">
-            <div className="sectionHeader">
-              <span>Versions</span>
-              <button className="addButton">
-                <AiOutlinePlus />
-              </button>
-            </div>
-            <div className="sidebarItem">
-              <FaHashtag />
-              <span>Scene Versions</span>
-            </div>
-            <div className="sidebarItem">
-              <FaHashtag />
-              <span>Scene Content</span>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
+    <SidePanelView
+      isOpen={isOpen}
+      isHovered={isHovered}
+      toggleSidePanel={toggleSidePanel}
+      scriptName={scriptName}
+      handleMouseEnter={handleMouseEnter}
+      handleMouseLeave={handleMouseLeave}
+    />
   );
 };
 
