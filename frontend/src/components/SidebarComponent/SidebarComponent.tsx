c8f4ba9bc370a830a9ff9ef1964132b54c892f0f
Mikel Giffin <giffinmike@hotmail.com>
2024-06-23 22:00:48 -0700

    chore: sidebar in progress 2

--- frontend/src/components/SidebarComponent/SidebarComponent.tsx
+++ frontend/src/components/SidebarComponent/SidebarComponent.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import "./sidebar.css";
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="sidebar">
-      <h2>Dashboard</h2>
+    <div className={`sidebar ${isOpen ? "open" : "collapsed"}`}>
+      <div className="toggleButtonContainer">
+        <button className="toggleButton" onClick={toggleSidebar}>
+          {isOpen ? <FaArrowLeft /> : <FaArrowRight />}
+        </button>
+      </div>
+      {isOpen && <h2>Dashboard</h2>}
     </div>
   );
 };
