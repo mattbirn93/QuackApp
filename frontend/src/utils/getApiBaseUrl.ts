e34a73197f49e8ff77b0c1a95676e3cd24618919
Mikel Giffin <giffinmike@hotmail.com>
2024-06-25 16:06:12 -0700

    environments set1

--- frontend/src/utils/getApiBaseUrl.ts
+++ frontend/src/utils/getApiBaseUrl.ts
@@ -1,18 +1,19 @@
-// export const getApiBaseUrl = () => {
-//   if (process.env.NODE_ENV === "production") {
-//     return import.meta.env.VITE_API_BASE_URL_PROD;
-//   } else {
-//     return import.meta.env.VITE_API_BASE_URL_DEV;
-//   }
-// };
+// src/utils/getApiBaseUrl.ts
+export const getApiBaseUrl = () => {
+  if (import.meta.env.MODE === "production") {
+    return import.meta.env.VITE_API_BASE_URL_PROD;
+  } else {
+    return import.meta.env.VITE_API_BASE_URL_DEV;
+  }
+};
 
 //////////////////
 
-export const getApiBaseUrl = () => {
-  const hostname = window.location.hostname;
-  if (hostname === "localhost" || hostname === "127.0.0.1") {
-    return import.meta.env.VITE_API_BASE_URL_DESKTOP;
-  } else {
-    return import.meta.env.VITE_API_BASE_URL_MOBILE;
-  }
-};
+// export const getApiBaseUrl = () => {
+//   const hostname = window.location.hostname;
+//   if (hostname === "localhost" || hostname === "127.0.0.1") {
+//     return import.meta.env.VITE_API_BASE_URL_DESKTOP;
+//   } else {
+//     return import.meta.env.VITE_API_BASE_URL_MOBILE;
+//   }
+// };
