fa7150683e1a1359ff3be48e8bfd108f62392104
Mikel Giffin <giffinmike@hotmail.com>
2024-06-25 12:52:36 -0700

    Ensure SSL certificates are created and used correctly

--- sw.js
+++ sw.js
@@ -1,30 +1,31 @@
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('my-cache').then((cache) => {
+    caches.open("my-cache").then((cache) => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192x192.png',
-        '/icon-512x512.png',
-        '/icon-maskable-512x512.png',
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/icon-192x192.png",
+        "/icon-512x512.png",
+        "/icon-maskable-512x512.png",
         // Add other assets to cache as needed
       ]);
-    })
+    }),
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request).catch(() => {
-        return caches.match('/index.html');
-      });
-    })
+      return (
+        response ||
+        fetch(event.request).catch(() => {
+          return caches.match("/index.html");
+        })
+      );
+    }),
   );
 });
-
-
 
 ///////////
 
