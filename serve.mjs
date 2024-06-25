d4941227d5ccfa39eaa8493409c47272a725165f
Mikel Giffin <giffinmike@hotmail.com>
2024-06-25 12:43:00 -0700

    Ensure SSL certificates are created and used correctly

--- serve.mjs
+++ serve.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import { readFileSync } from "fs";
 import { resolve } from "path";
 import { createServer } from "https";
+import http from "http";
 import dotenv from "dotenv";
 
 dotenv.config(); // Load environment variables from .env file
@@ -9,18 +10,7 @@ dotenv.config(); // Load environment variables from .env file
 const keyPath = process.env.VITE_SSL_KEY_PATH;
 const certPath = process.env.VITE_SSL_CERT_PATH;
 
-if (!keyPath || !certPath) {
-  throw new Error(
-    "SSL key and certificate paths must be defined in the .env file",
-  );
-}
-
 const app = express();
-const options = {
-  key: readFileSync(resolve(keyPath)),
-  cert: readFileSync(resolve(certPath)),
-};
-
 const port = process.env.PORT || 5173;
 const publicPath = resolve("dist");
 
@@ -32,9 +22,19 @@ app.get("*", (req, res) => {
   res.sendFile(resolve(publicPath, "index.html"));
 });
 
-createServer(options, app).listen(port, () => {
-  console.log(`Server is running on https://localhost:${port}`);
-});
+if (keyPath && certPath) {
+  const options = {
+    key: readFileSync(resolve(keyPath)),
+    cert: readFileSync(resolve(certPath)),
+  };
+  createServer(options, app).listen(port, "0.0.0.0", () => {
+    console.log(`Server is running on https://localhost:${port}`);
+  });
+} else {
+  http.createServer(app).listen(port, "0.0.0.0", () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
 
 /////////////////////////////////
 
