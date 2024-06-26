f306358755bd5abdb45b20a814510946b3791129
Mikel Giffin <giffinmike@hotmail.com>
2024-06-25 17:41:00 -0700

    working1

--- frontend/src/utils/getApiBaseUrl.ts
+++ frontend/src/utils/getApiBaseUrl.ts
@@ -1,19 +1,19 @@
 // src/utils/getApiBaseUrl.ts
-export const getApiBaseUrl = () => {
-  if (import.meta.env.MODE === "development") {
-    return import.meta.env.VITE_API_BASE_URL_PROD;
-  } else {
-    return import.meta.env.VITE_API_BASE_URL_DEV;
-  }
-};
+// export const getApiBaseUrl = () => {
+//   if (import.meta.env.MODE === "development") {
+//     return import.meta.env.VITE_API_BASE_URL_PROD;
+//   } else {
+//     return import.meta.env.VITE_API_BASE_URL_DEV;
+//   }
+// };
 
 //////////////////
 
-// export const getApiBaseUrl = () => {
-//   const hostname = window.location.hostname;
-//   if (hostname === "localhost" || hostname === "127.0.0.1") {
-//     return import.meta.env.VITE_API_BASE_URL_DEV;
-//   } else {
-//     return import.meta.env.VITE_API_BASE_URL_PROD;
-//   }
-// };
+export const getApiBaseUrl = () => {
+  const hostname = window.location.hostname;
+  if (hostname === "localhost" || hostname === "127.0.0.1") {
+    return import.meta.env.VITE_API_BASE_URL_DEV;
+  } else {
+    return import.meta.env.VITE_API_BASE_URL_PROD;
+  }
+};
