2b9fbfdab716ccb691b3e312f71771fd5d4f5fa4
Mikel Giffin <giffinmike@hotmail.com>
2024-06-25 21:34:02 -0700

    no server1

--- frontend/src/serviceWorkerRegistration.ts
+++ frontend/src/serviceWorkerRegistration.ts
@@ -38,6 +38,9 @@ export function register(config?: Config) {
   }
 }
 
+// Print swUrl to verify its correctness in the browser's console.
+console.log(`Service Worker URL being registered: ${swUrl}`);
+
 function registerValidSW(swUrl: string, config?: Config) {
   navigator.serviceWorker
     .register(swUrl)
