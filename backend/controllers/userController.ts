c3dbd77dbda1819c76c12ad177edf77fbbc8e0ad
Mikel Giffin <giffinmike@hotmail.com>
2024-06-26 17:34:58 -0700

    adfsdf

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -21,23 +21,60 @@ export const createUser = async (req: Request, res: Response) => {
 export const fetchUserById = async (req: Request, res: Response) => {
   const { id } = req.query;
   if (typeof id !== "string") {
-    return res.status(400).json({ message: "Invalid user ID" });
+    return res.status(400).json({ message: "CHECK ME Invalid user ID" });
   }
-  console.log("Attempting to fetch user with ID:", id); // Confirm receipt and handling of ID
+  console.log("CHECK ME Attempting to fetch user with ID:", id); // Confirm receipt and handling of ID
   try {
     const user = await UserService.getUserById(id);
     if (!user) {
-      console.error("User not found with ID:", id);
-      return res.status(404).json({ message: "User not found" });
+      console.error("CHRECK ME User not found with ID:", id);
+      return res.status(404).json({ message: "CHRECK ME User not found" });
     }
-    console.log("User fetched successfully:", user); // Log user object
+    console.log("CHECK ME User fetched successfully:", user); // Log user object
     res.setHeader("Content-Type", "application/json");
     res.json(user);
   } catch (error: any) {
-    console.error("Error fetching user:", error.message); // Log error
+    console.error("CHECK ME Error fetching user:", error.message); // Log error
     res.status(500).json({ message: error.message });
   }
 };
+
+/////////////////////
+
+// import { Request, Response } from "express";
+// import UserService from "../services/userService.js";
+
+// export const createUser = async (req: Request, res: Response) => {
+//   const { first_name, last_name, email, scripts_id_array } = req.body;
+
+//   try {
+//     const savedUser = await UserService.createUser({
+//       first_name,
+//       last_name,
+//       email,
+//       scripts_id_array,
+//     });
+//     res.status(201).json(savedUser);
+//   } catch (error: any) {
+//     res.status(400).json({ message: error.message });
+//   }
+// };
+
+// export const fetchUserById = async (req: Request, res: Response) => {
+//   const { id } = req.query;
+//   if (typeof id !== "string") {
+//     return res.status(400).json({ message: "Invalid user ID" });
+//   }
+//   console.log("Attempting to fetch user with ID:", id); // Confirm receipt and handling of ID
+//   try {
+//     const user = await UserService.getUserById(id);
+//     console.log("User fetched successfully:", user); // Log user object
+//     res.json(user);
+//   } catch (error: any) {
+//     console.log("Error fetching user:", error.message); // Log error
+//     res.status(500).json({ message: error.message });
+//   }
+// };
 
 /////////////////////////////////
 
