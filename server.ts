1d2fd943501007bb92eae452f65b1e2f18d91a3b
Mikel Giffin <giffinmike@hotmail.com>
2024-06-27 14:13:28 -0700

    asdf

--- server.ts
+++ server.ts
@@ -114,7 +114,6 @@ import { Server as SocketIOServer } from "socket.io";
 
 // Load environment variables from .env file
 dotenv.config();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
