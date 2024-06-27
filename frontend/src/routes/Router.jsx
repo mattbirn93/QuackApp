2296c54879cb18ceebb14d8f731d50c5c28098be
Mikel Giffin <giffinmike@hotmail.com>
2024-06-26 20:16:19 -0700

    adsf

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -41,3 +41,49 @@ const routes = [
 ];
 
 export const router = createBrowserRouter(routes);
+
+//////////////////////
+
+// import React from "react";
+// import { createBrowserRouter } from "react-router-dom";
+// import MainLayout from "../layouts/MainLayout";
+// import App from "../App";
+// import ScriptsLibraryView from "../views/ScriptsLibraryView/ScriptsLibraryView";
+// import NotFound from "../views/NotFound";
+
+// const routes = [
+//   {
+//     path: "/",
+//     element: (
+//       <MainLayout>
+//         <App />
+//       </MainLayout>
+//     ),
+//   },
+//   {
+//     path: "/scriptslibrary",
+//     element: (
+//       <MainLayout>
+//         <ScriptsLibraryView />
+//       </MainLayout>
+//     ),
+//   },
+//   {
+//     path: "/app/:scriptId",
+//     element: (
+//       <MainLayout>
+//         <App />
+//       </MainLayout>
+//     ),
+//   },
+//   {
+//     path: "*",
+//     element: (
+//       <MainLayout>
+//         <NotFound />
+//       </MainLayout>
+//     ),
+//   },
+// ];
+
+// export const router = createBrowserRouter(routes);
