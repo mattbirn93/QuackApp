e8ab433b3398aa73d921f89fb40766f5ad9fa639
Mikel Giffin <giffinmike@hotmail.com>
2024-06-27 21:26:19 -0700

    asdfsdfsdf

--- frontend/src/serviceWorkerRegistration.ts
+++ frontend/src/serviceWorkerRegistration.ts
@@ -11,10 +11,14 @@ type Config = {
   onUpdate?: (registration: ServiceWorkerRegistration) => void;
 };
 
+function sanitizeURL(url: string): string {
+  return url.replace(/\/+$/, ""); // Remove trailing slashes
+}
+
 export function register(config?: Config) {
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
     const publicUrl = new URL(
-      import.meta.env.VITE_PUBLIC_URL || ".", // Provide a fallback value here
+      sanitizeURL(import.meta.env.VITE_PUBLIC_URL || "."),
       window.location.href,
     );
 
@@ -35,7 +39,7 @@ export function register(config?: Config) {
       return;
     }
     window.addEventListener("load", () => {
-      const swUrl = `${import.meta.env.VITE_PUBLIC_URL || "."}/sw.js`; // Ensure proper path
+      const swUrl = `${sanitizeURL(import.meta.env.VITE_PUBLIC_URL || ".")}/sw.js`; // Ensure proper path
       console.log("WELCOME TURKEY Service worker URL:", swUrl); // Log swUrl
 
       if (isLocalhost) {
@@ -125,6 +129,136 @@ export function unregister() {
       });
   }
 }
+
+////////
+
+// const isLocalhost = Boolean(
+//   window.location.hostname === "localhost" ||
+//   window.location.hostname === "[::1]" ||
+//   window.location.hostname.match(
+//     /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/,
+//   ),
+// );
+
+// type Config = {
+//   onSuccess?: (registration: ServiceWorkerRegistration) => void;
+//   onUpdate?: (registration: ServiceWorkerRegistration) => void;
+// };
+
+// export function register(config?: Config) {
+//   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
+//     const publicUrl = new URL(
+//       import.meta.env.VITE_PUBLIC_URL || ".", // Provide a fallback value here
+//       window.location.href,
+//     );
+
+//     console.log(
+//       "WELCOME TURKEY PUBLIC_URL:",
+//       import.meta.env.VITE_PUBLIC_URL || ".",
+//     ); // Log PUBLIC_URL with fallback
+//     console.log("WELCOME TURKEY publicUrl.origin:", publicUrl.origin); // Log publicUrl.origin
+//     console.log(
+//       "WELCOME TURKEY window.location.origin:",
+//       window.location.origin,
+//     ); // Log window.location.origin
+
+//     if (publicUrl.origin !== window.location.origin) {
+//       console.log(
+//         "WELCOME TURKEY Service worker not registered due to origin mismatch.",
+//       );
+//       return;
+//     }
+//     window.addEventListener("load", () => {
+//       const swUrl = `${import.meta.env.VITE_PUBLIC_URL || "."}/sw.js`; // Ensure proper path
+//       console.log("WELCOME TURKEY Service worker URL:", swUrl); // Log swUrl
+
+//       if (isLocalhost) {
+//         checkValidServiceWorker(swUrl, config);
+//         navigator.serviceWorker.ready.then(() => {
+//           console.log(
+//             "WELCOME TURKEY This web app is being served cache-first by a service worker. To learn more, visit https://cra.link/PWA",
+//           );
+//         });
+//       } else {
+//         registerValidSW(swUrl, config);
+//       }
+//     });
+//   }
+// }
+
+// function registerValidSW(swUrl: string, config?: Config) {
+//   navigator.serviceWorker
+//     .register(swUrl)
+//     .then((registration) => {
+//       registration.onupdatefound = () => {
+//         const installingWorker = registration.installing;
+//         if (installingWorker == null) {
+//           return;
+//         }
+//         installingWorker.onstatechange = () => {
+//           if (installingWorker.state === "installed") {
+//             if (navigator.serviceWorker.controller) {
+//               console.log(
+//                 "WELCOME TURKEY New content is available and will be used when all tabs for this page are closed. See https://cra.link/PWA.",
+//               );
+//               if (config && config.onUpdate) {
+//                 config.onUpdate(registration);
+//               }
+//             } else {
+//               console.log("WELCOME TURKEY Content is cached for offline use.");
+//               if (config && config.onSuccess) {
+//                 config.onSuccess(registration);
+//               }
+//             }
+//           }
+//         };
+//       };
+//     })
+//     .catch((error) => {
+//       console.error(
+//         "WELCOME TURKEY Error during service worker registration:",
+//         error,
+//       );
+//     });
+// }
+
+// function checkValidServiceWorker(swUrl: string, config?: Config) {
+//   fetch(swUrl, {
+//     headers: { "Service-Worker": "script" },
+//   })
+//     .then((response) => {
+//       const contentType = response.headers.get("content-type");
+//       if (
+//         response.status === 404 ||
+//         (contentType != null && contentType.indexOf("javascript") === -1)
+//       ) {
+//         navigator.serviceWorker.ready.then((registration) => {
+//           registration.unregister().then(() => {
+//             window.location.reload();
+//           });
+//         });
+//       } else {
+//         registerValidSW(swUrl, config);
+//       }
+//     })
+//     .catch(() => {
+//       console.log(
+//         "WELCOME TURKEY No internet connection found. App is running in offline mode.",
+//       );
+//     });
+// }
+
+// export function unregister() {
+//   if ("serviceWorker" in navigator) {
+//     navigator.serviceWorker.ready
+//       .then((registration) => {
+//         registration.unregister();
+//       })
+//       .catch((error) => {
+//         console.error(error.message);
+//       });
+//   }
+// }
 
 /////////////////
 
