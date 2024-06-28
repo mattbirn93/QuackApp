be5c233947065a016d213ab3c638064a3e493e7d
Mikel Giffin <giffinmike@hotmail.com>
2024-06-28 11:46:41 -0700

    afdfd3

--- serve.mjs
+++ serve.mjs
@@ -85,8 +85,20 @@ app.get("/api/dog", (req, res) => {
   res.json({ message: "Woof!" });
 });
 
+import userRoutes from "./backend/routes/userRoutes.js";
+import scriptRoutes from "./backend/routes/scriptRoutes.js";
+import sceneRoutes from "./backend/routes/sceneRoutes.js";
+import sceneVersionRoutes from "./backend/routes/sceneVersionRoutes.js";
+import sceneVersionContentRoutes from "./backend/routes/sceneVersionContentRoutes.js";
+
+app.use("/api/users", userRoutes);
+app.use("/api/scripts", scriptRoutes);
+app.use("/api/scenes", sceneRoutes);
+app.use("/api/sceneVersions", sceneVersionRoutes);
+app.use("/api/sceneVersionContent", sceneVersionContentRoutes);
+
 // Serve index.html on all other routes to support client-side routing
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(resolve(publicPath, "index.html"));
 });
 
