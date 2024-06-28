1d16be5b898891582dc35c7dcd14c0b21b95786a
Mikel Giffin <giffinmike@hotmail.com>
2024-06-27 17:08:18 -0700

    asdf

--- server.ts
+++ server.ts
@@ -50,7 +50,7 @@ app.use("/api/sceneVersionContent", sceneVersionContentRoutes);
 app.use(express.static(path.join(__dirname, "dist")));
 
 // Catch-all route - this should be the last route
-app.get("/", (req, res) => {
+app.get(".", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
