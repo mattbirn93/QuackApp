441430dfec3a1f19259e942c2db94b93293ec042
Mikel Giffin <giffinmike@hotmail.com>
2024-06-29 16:40:52 -0700

    added in Hux fixes temp1

--- frontend/src/utils/getApiBaseUrl.ts
+++ frontend/src/utils/getApiBaseUrl.ts
@@ -1,16 +1,15 @@
 export const getApiBaseUrl = () => {
-  const hostname = window.location.hostname;
+  //const hostname = window.location.hostname;
   const apiUrl =
-    hostname === "localhost" || hostname === "127.0.0.1"
-      ? import.meta.env.VITE_API_BASE_URL_DESKTOP
-      : import.meta.env.VITE_API_BASE_URL_MOBILE;
+    process.env.VITE_API_BASE_URL_DESKTOP ||
+    process.env.VITE_API_BASE_URL_MOBILE;
   console.log(
     "POOPSIE VITE_API_BASE_URL_DESKTOP:",
-    import.meta.env.VITE_API_BASE_URL_DESKTOP,
+    process.env.VITE_API_BASE_URL_DESKTOP,
   );
   console.log(
     "POOPSIE VITE_API_BASE_URL_MOBILE:",
-    import.meta.env.VITE_API_BASE_URL_MOBILE,
+    process.env.VITE_API_BASE_URL_MOBILE,
   );
   console.log(`POOPSIE Determined API Base URL: ${apiUrl}`);
   return apiUrl;
@@ -19,6 +18,30 @@ export const getApiBaseUrl = () => {
 // Inside the component
 const API_BASE_URL = getApiBaseUrl();
 console.log(`POOPSIE LOOK THERE Using API Base URL: ${API_BASE_URL}`);
+
+///////////
+
+// export const getApiBaseUrl = () => {
+//   const hostname = window.location.hostname;
+//   const apiUrl =
+//     hostname === "localhost" || hostname === "127.0.0.1"
+//       ? import.meta.env.VITE_API_BASE_URL_DESKTOP
+//       : import.meta.env.VITE_API_BASE_URL_MOBILE;
+//   console.log(
+//     "POOPSIE VITE_API_BASE_URL_DESKTOP:",
+//     import.meta.env.VITE_API_BASE_URL_DESKTOP,
+//   );
+//   console.log(
+//     "POOPSIE VITE_API_BASE_URL_MOBILE:",
+//     import.meta.env.VITE_API_BASE_URL_MOBILE,
+//   );
+//   console.log(`POOPSIE Determined API Base URL: ${apiUrl}`);
+//   return apiUrl;
+// };
+
+// // Inside the component
+// const API_BASE_URL = getApiBaseUrl();
+// console.log(`POOPSIE LOOK THERE Using API Base URL: ${API_BASE_URL}`);
 
 /////////////////////
 
