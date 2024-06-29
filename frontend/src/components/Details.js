c6f39ed9318c0f88d199093d1dec26a9881ddd39
Mikel Giffin <giffinmike@hotmail.com>
2024-06-28 22:55:31 -0700

    post revert3

--- frontend/src/components/Details.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment } from "react/jsx-runtime";
-import React from "react";
-import parse from "html-react-parser";
-const Details = ({ description }) => {
-    return (_jsx(_Fragment, { children: _jsx("div", { className: "ProseMirror", children: parse(description) }) }));
-};
-export default Details;
