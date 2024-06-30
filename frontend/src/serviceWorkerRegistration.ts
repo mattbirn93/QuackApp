bc633dc05d403d667805be03369d122d1681c5f2
Mikel Giffin <giffinmike@hotmail.com>
2024-06-29 22:18:42 -0700

    aaa

--- frontend/src/serviceWorkerRegistration.ts
+++ frontend/src/serviceWorkerRegistration.ts
@@ -7,17 +7,14 @@ export function register(config?: Config) {
   // if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
   window.addEventListener("load", async () => {
     try {
-      const publicUrl = new URL(
-        import.meta.env.VITE_PUBLIC_URL || ".", // Provide a fallback value here
-        window.location.href,
-      );
+      const publicUrl = new URL(import.meta.env.VITE_PUBLIC_URL);
 
       console.log("PUBLIC_URL:", import.meta.env.VITE_PUBLIC_URL || "."); // Log PUBLIC_URL with fallback
       console.log("publicUrl.origin:", publicUrl.origin); // Log publicUrl.origin
       console.log("window.location.origin:", window.location.origin); // Log window.location.origin
 
       // Only proceed with service worker registration if origins match
-      if (publicUrl.origin === window.location.origin) {
+      if (publicUrl.origin) {
         const swUrl = `${publicUrl.origin}/sw.js`; // Ensure proper path
         console.log("Service worker URL:", swUrl); // Log swUrl
 
