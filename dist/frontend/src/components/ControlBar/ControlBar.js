c5cc7c808c58f1c81b70193d1ade75c780d331be
Mikel Giffin <giffinmike@hotmail.com>
2024-06-30 10:59:46 -0700

    more script changes

--- dist/frontend/src/components/ControlBar/ControlBar.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const ControlBar = () => {
-    return _jsx("div", { style: { color: "white" }, children: "ControlBar" });
-};
-export default ControlBar;
