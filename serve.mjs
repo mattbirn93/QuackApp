967541e83cefb028604420df4580260911151281
Mikel Giffin <giffinmike@hotmail.com>
2024-06-30 00:02:12 -0700

    revert5

--- serve.mjs
+++ serve.mjs
@@ -41,7 +41,7 @@ app.use((err, req, res) => {
   res.status(500).send("Something broke!");
 });
 
-const PORT = process.env.PORT || 5173;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
