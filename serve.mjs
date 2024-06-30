90f776d700098f9aaabc535827a15d998e219430
Mikel Giffin <giffinmike@hotmail.com>
2024-06-30 00:07:06 -0700

    revert5

--- serve.mjs
+++ serve.mjs
@@ -30,7 +30,7 @@ app.get("/api/scenes", (req, res) => {
   res.json({ scenes: "scene data" });
 });
 
-// All other GET requests not handled before will return the fronend app
+// All other GET requests not handled before wills return the fronend app
 app.get("*", (req, res) => {
   res.sendFile(join(__dirname, "dist", "index.html"));
 });
