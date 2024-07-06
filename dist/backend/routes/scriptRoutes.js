9c994a32587e2ce2106e7920c771cf3227c0e0ea
Mikel Giffin <giffinmike@hotmail.com>
2024-07-05 22:11:35 -0700

    stopping for night, kinda works

--- dist/backend/routes/scriptRoutes.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import { fetchScripts, } from '../controllers/scriptController.js';
-const router = express.Router();
-router.get('/', fetchScripts);
-export default router;
