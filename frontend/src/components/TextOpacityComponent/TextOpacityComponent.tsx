4a9f70fc8271049f2d13283c150ffc37871afc42
Mikel Giffin <giffinmike@hotmail.com>
2024-07-14 19:07:33 -0700

    added scrolling opacity text2

--- frontend/src/components/TextOpacityComponent/TextOpacityComponent.tsx
+++ frontend/src/components/TextOpacityComponent/TextOpacityComponent.tsx
@@ -1,19 +1,16 @@
 import React from "react";
-import Word from "./Word";
 import Character from "./Character";
+import styles from "./TextOpacityComponent.module.css";
 
 const paragraph =
   "  Meet Quack!, a cutting-edge mobile screenwriting app designed for the modern screenwriter on the go. Seamlessly blending functionality and convenience, Quack! allows users to craft edit, and collaborate on scripts directly from their mobile devices. Whether you’re brainstorming ideas, writing dialogue, or revising scenes, Quack! provides a user-friendly interface that makes screenwriting a breeze.";
 
 const TextOpacityComponent = () => {
-  const words = paragraph.split(" ");
   return (
-    <main>
-      <div style={{ height: "100vh" }}></div>
-      <Word paragraph={paragraph} />
-      <div style={{ height: "100vh" }}></div>
-      <Character paragraph={paragraph} />
-      <div style={{ height: "100vh" }}></div>
+    <main className={styles.textOpacityWrapper}>
+      <div className={styles.textOpacityContainer}>
+        <Character paragraph={paragraph} />
+      </div>
     </main>
   );
 };
